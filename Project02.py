23cba499c95618374ae5a2c039f0a8823b6d3c4f
Maithili Deshmukh <maithilirssd@gmail.com>
2020-09-21 00:30:35 -0400

    minor changes and added comments

--- Project02.py
+++ Project02.py
@@ -1,56 +1,76 @@
-validTagsArr =  ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE"]
+# list of all valid tags
+validTags =  ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE"]
 
+# function to concatenate agruments with length greater than 1
 def concat(eachWord):
         eachWord[2] = eachWord[2] + " " + eachWord[3]
         del eachWord[3]
         if(len(eachWord) != 3):
             concat(eachWord)
-        
-def loadFile(fname):
+
+# function to print data as per format specified in "Project 02"
+def printData(fname):
     file = open(fname)
-    lineArr = []
-    fullLines = []
+    splitInputLines = []
+    inputLines = []
+    
     for line in file:
-        fullLines.append(line.strip())
-        lineArr.append(line.strip().split(' '))
-    for eachWord in lineArr:
+        inputLines.append(line.strip())
+        splitInputLines.append(line.strip().split(' '))
+        
+    for eachWord in splitInputLines:
         if(len(eachWord) > 3):
             concat(eachWord)
-    for i, line in enumerate(lineArr):
-        print('--> ' + fullLines[i])
-        if lineArr[i][0] == '1' or lineArr[i][0] == '2':
-            if lineArr[i][1] in validTagsArr:
-                if len(lineArr[i]) > 2:
+            
+    for i, line in enumerate(splitInputLines):
+        print('--> ' + inputLines[i])
+        
+        # for tag = 1 and tag = 2
+        if splitInputLines[i][0] == '1' or splitInputLines[i][0] == '2':
+            # if tags are valid
+            if splitInputLines[i][1] in validTags:
+                if len(splitInputLines[i]) > 2:
                     print('<--', end = " ")
-                    print(lineArr[i][0] + '|' + lineArr[i][1] + '|Y|' + lineArr[i][2])
-                elif len(lineArr[i]) <= 2:
+                    print(splitInputLines[i][0] + '|' + splitInputLines[i][1] + '|Y|' + splitInputLines[i][2])
+                elif len(splitInputLines[i]) <= 2:
                     print('<--', end = " ")
-                    print(lineArr[i][0] + '|' + lineArr[i][1] + '|Y')
+                    print(splitInputLines[i][0] + '|' + splitInputLines[i][1] + '|Y')
+            # if tags are invalid
             else:
-                if len(lineArr[i]) < 2:
+                if len(splitInputLines[i]) < 2:
                     print('<--', end = " ")
-                    print(lineArr[i][0] + '|' + lineArr[i][1] + '|N|' + lineArr[i][2])
+                    print(splitInputLines[i][0] + '|' + splitInputLines[i][1] + '|N|' + splitInputLines[i][2])
                 else:
                     print('<--', end = " ")
-                    print(lineArr[i][0] + '|' + lineArr[i][1] + '|N')
-        if lineArr[i][0] == '0':
-            if len(lineArr[i]) > 2:
-                if lineArr[i][2] in validTagsArr:
+                    print(splitInputLines[i][0] + '|' + splitInputLines[i][1] + '|N')
+                    
+        # for tag = 0
+        if splitInputLines[i][0] == '0':
+            if len(splitInputLines[i]) > 2:
+                # if tags are valid
+                # check for "FAM", "INDI" tags
+                if splitInputLines[i][2] in validTags:
                     print('<--', end = " ")
-                    print(lineArr[i][0] + '|' + lineArr[i][2]+ '|Y|' + lineArr[i][1])
-                elif lineArr[i][1] in validTagsArr:
+                    print(splitInputLines[i][0] + '|' + splitInputLines[i][2]+ '|Y|' + splitInputLines[i][1])
+                # check for "NOTE" tags
+                elif splitInputLines[i][1] in validTags:
                     print('<--', end = " ")
-                    print(lineArr[i][0] + '|' + lineArr[i][1]+ '|Y|' + lineArr[i][2])
+                    print(splitInputLines[i][0] + '|' + splitInputLines[i][1]+ '|Y|' + splitInputLines[i][2])
+                # if tags are invalid
                 else:
                     print('<--', end = " ")
-                    print(lineArr[i][0] + '|' + lineArr[i][1] + '|N|' + lineArr[i][2])
-            elif len(lineArr[i]) <= 2:
-                if lineArr[i][1] in validTagsArr:
+                    print(splitInputLines[i][0] + '|' + splitInputLines[i][1] + '|N|' + splitInputLines[i][2])
+            elif len(splitInputLines[i]) <= 2:
+                # if tags are valid
+                # check for "TRLR" tags
+                if splitInputLines[i][1] in validTags:
                     print('<--', end = " ")
-                    print(lineArr[i][0] + '|' + lineArr[i][1] + '|Y')
+                    print(splitInputLines[i][0] + '|' + splitInputLines[i][1] + '|Y')
+                # if tags are invalid
                 else:
                     print('<--', end = " ")
-                    print(lineArr[i][0] + '|' + lineArr[i][1] + '|N')
+                    print(splitInputLines[i][0] + '|' + splitInputLines[i][1] + '|N')
     file.close()
-    
-file = loadFile('test_GEDCOM_file.ged');
+
+# use "printData" function on a sample file
+file = printData('test_GEDCOM_file.ged');
