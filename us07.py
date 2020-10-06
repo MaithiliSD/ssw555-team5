f857b8ca612c2d96fbb8e868728589b867f7ba92
Maithili Deshmukh <maithilirssd@gmail.com>
2020-10-05 21:38:33 -0400

    added user story 7

---
+++ us07.py
@@ -0,0 +1,21 @@
+from project03 import indi_list,fam_list,calculateAge,splitBdate,isAlive
+
+
+def US07(): #  US07 Less then 150 years old
+    result= False  
+    
+
+    for i in (indi_list):
+        if (calculateAge(splitBdate(i[3])) < 150):
+            print("INDIVIDUAL: US07: "+" Less than 150 years old: Birth date "+ i[3])
+            result = True
+   
+    return result
+
+            
+def main()->None:
+   
+    US07()
+
+if __name__ == "__main__":
+    main()
