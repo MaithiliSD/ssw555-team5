4114ca98efc8243bbfa2259779dfec00c7e9665e
Maithili Deshmukh <maithilirssd@gmail.com>
2020-11-25 16:32:45 -0500

    test case for user story 25

---
+++ testcases_25.py
@@ -0,0 +1,10 @@
+import unittest
+import us25
+
+class TestUserStoriestwo(unittest.TestCase):
+        
+    def test_unique_names_1(self):
+        list1 = []
+        self.assertTrue(us25.are_names_uniques is list1)
+
+unittest.main()
